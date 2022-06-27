e3532e67a443a0f5ec9d5d869a59da010bc0b65e
LukasG <l.grazevicius@gmail.com>
2022-06-27 14:49:50 +0300

    Add Model Class For API

---
+++ app/src/main/java/lt/vcs/movieappbd/models/MovieModel.java
@@ -0,0 +1,43 @@
+package lt.vcs.movieappbd.models;
+
+public class MovieModel {
+    private String title;
+    private String poster_path;
+    private String release_date;
+    private int movie_id;
+    private float vote_average;
+    private String movie_overview;
+
+    public MovieModel(String title, String poster_path, String release_date, int movie_id, float vote_average, String movie_overview) {
+        this.title = title;
+        this.poster_path = poster_path;
+        this.release_date = release_date;
+        this.movie_id = movie_id;
+        this.vote_average = vote_average;
+        this.movie_overview = movie_overview;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public float getVote_average() {
+        return vote_average;
+    }
+
+    public String getMovie_overview() {
+        return movie_overview;
+    }
+}
