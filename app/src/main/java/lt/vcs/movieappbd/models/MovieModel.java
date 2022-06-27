a74bbadf45caeb59aee0db6d6fca01935e9a9107
LukasG <l.grazevicius@gmail.com>
2022-06-27 14:53:00 +0300

    Updated Model Class

--- app/src/main/java/lt/vcs/movieappbd/models/MovieModel.java
+++ app/src/main/java/lt/vcs/movieappbd/models/MovieModel.java
@@ -1,6 +1,9 @@
 package lt.vcs.movieappbd.models;
 
-public class MovieModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieModel implements Parcelable {
     private String title;
     private String poster_path;
     private String release_date;
@@ -16,6 +19,27 @@ public class MovieModel {
         this.vote_average = vote_average;
         this.movie_overview = movie_overview;
     }
+
+    protected MovieModel(Parcel in) {
+        title = in.readString();
+        poster_path = in.readString();
+        release_date = in.readString();
+        movie_id = in.readInt();
+        vote_average = in.readFloat();
+        movie_overview = in.readString();
+    }
+
+    public static final Creator<MovieModel> CREATOR = new Creator<MovieModel>() {
+        @Override
+        public MovieModel createFromParcel(Parcel in) {
+            return new MovieModel(in);
+        }
+
+        @Override
+        public MovieModel[] newArray(int size) {
+            return new MovieModel[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -39,5 +63,20 @@ public class MovieModel {
 
     public String getMovie_overview() {
         return movie_overview;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(poster_path);
+        parcel.writeString(release_date);
+        parcel.writeInt(movie_id);
+        parcel.writeFloat(vote_average);
+        parcel.writeString(movie_overview);
     }
 }
